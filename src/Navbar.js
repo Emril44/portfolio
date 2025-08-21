397cfa934dca3aaf5cb63b8f840f360ba6f2c631
M. K. <42112702+Emril44@users.noreply.github.com>
2025-08-21 22:13:38 +0300

    set up basics

---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="logo-ig-idk-lol">
+                <p>Max's Portfolio</p>
+            </div>
+            <div className="nav-buttons">
+                <ul className="work-button-list">
+                    <li>Projects</li>
+                    <li>Certifications</li>
+                </ul>
+            </div>
+            <div className="buttons-maybe">
+                <ul className="personal-buttons">
+                    <li>About Me</li>
+                    <li>Contacts</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
