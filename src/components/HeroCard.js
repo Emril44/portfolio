ef34cc6adc65641c60824334002a47f097cc3540
M. K. <42112702+Emril44@users.noreply.github.com>
2025-10-01 22:08:48 +0300

    First version of Hero

--- src/components/HeroCard.js
+++ src/components/HeroCard.js
@@ -4,9 +4,9 @@ import "./HeroCard.css";
 const HeroCard = ({ children }) => {
     return (
         <div className="hero-card">
-            <svg className="hero-border" viewBox="0 0 600 400" preserveAspectRatio="none">
+            <svg className="hero-border" viewBox="0 0 1600 700" preserveAspectRatio="none">
                 <polygon
-                    points="0,0 600,0 600,400 40,400 0,345"
+                    points="0,0 1600,0 1600,700 90,700 0,580"
                     fill="rgba(0,0,0,0.6)"
                     stroke="#c9346d"
                     strokeWidth="2"
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,7 @@
 import Canvas from "../components/Canvas";
 import HeroCard from "../components/HeroCard";
+import "./Main.css"
+import myface from "../assets/myface.jpg";
 
 function Main() {
     return (
@@ -10,13 +12,23 @@ function Main() {
                 zIndex: 1,
                 display: "flex",
                 justifyContent: "center",
-                alignItems: "flex-start", /* so it doesn’t stick to the very center vertically */
+                alignItems: "flex-start",
                 minHeight: "100vh",
-                paddingBottom: "1em" /* push down below navbar */
             }}>
                 <HeroCard>
-                    <h1>Hello, I’m a dev</h1>
-                    <p>feef</p>
+                    <div className="hero-layout">
+                        <img src={myface} alt="Portrait of Max" className="portrait" />
+                        <div className="hero-text">
+                            <h1 className="hero-header1">Hello, I’m Max.</h1>
+                            <h2 className="hero-header2">I build clean, efficient, and creative solutions in web development.</h2>
+                            <p className="hero-subtitle">I'm adept at Java (Spring Boot) and JavaScript (React, Vue).</p>
+                            <button className="call-to-action">
+                                <div className="call-to-action__content">View my work_</div>
+                                <span className="call-to-action__glitch"></span>
+                                <span className="call-to-action__label">mk44</span>
+                            </button>
+                        </div>
+                    </div>
                 </HeroCard>
             </div>
         </div>
